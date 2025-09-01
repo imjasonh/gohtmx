188f20f48d88f33bcffa0e801fafa26e1e9d306f
Jason Hall <jason@chainguard.dev>
2025-09-01 09:30:20 -0400

    use flags
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"flag"
 	"gohtmx/internal/app"
 	"log/slog"
 	"net/http"
 )
 
+var (
+	file = flag.String("file", "todos.jsonl", "Path to the JSONL file to store todos")
+	port = flag.String("port", "8080", "Port to run the server on")
+)
+
 func main() {
+	flag.Parse()
+
 	// Create the application
-	application := app.New("todos.jsonl")
+	application := app.New(*file)
 
 	// Set up routes
 	mux := http.NewServeMux()
 	application.SetupRoutes(mux)
 
 	// Start server
-	port := ":8080"
-	slog.Info("Starting server", "port", port)
-	if err := http.ListenAndServe(port, mux); err != nil {
+
+	slog.Info("Starting server", "port", *port)
+	if err := http.ListenAndServe(":"+*port, mux); err != nil {
 		slog.Error("Server failed to start", "error", err)
 	}
 }
